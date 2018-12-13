472d876e718e7314b967f151b1e855aa6d03f57a
Paris Dolfman <parisdolfman@gmail.com>
2018-12-13 16:49:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // add solution here
  function theBeatlesPlay(musicians, instruments) { // function takes in argument of two arrays, musicians && instruments
-   var array = []; // empty array 
-    for (var counter = 0; counter < musicians.length; counter++) { // setting counter to equal 0 temporarily; on condition that the counter # is less than the legnth of the musicians array execute code block; 
+   var array = []; // empty array
+    for (var counter = 0; counter < musicians.length; counter++) { // setting counter to equal 0 temporarily; on condition that the counter # is less than the legnth of the musicians array execute code block (can use either musicians or instruments); then increment counter to account for each execution;
       array.push(`${musicians[counter]} plays ${instruments[counter]}`)
     }
     return array
+ }
+
+
+ function johnLennonFacts(facts) {
+   vary array = [];
+   while(facts.length > 0) {
+     
+   }
  }
