90d66050ac67e6af71bd69d5fe98fd317bf55922
Paris Dolfman <parisdolfman@gmail.com>
2018-12-13 16:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
- function theBeatlesPlay(musicians, instruments) {
-   var array = [];
-    for (var counter = 0; counter < musicians.length; counter++) {
+ function theBeatlesPlay(musicians, instruments) { // function takes in argument of two arrays, musicians && instruments
+   var array = []; // empty array 
+    for (var counter = 0; counter < musicians.length; counter++) { // setting counter to equal 0 temporarily; on condition that the counter # is less than the legnth of the musicians array execute code block; 
       array.push(`${musicians[counter]} plays ${instruments[counter]}`)
     }
     return array
